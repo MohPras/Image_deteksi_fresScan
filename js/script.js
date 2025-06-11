11f665bfdc6bfb499f4da9bf816ead2f57920a8c
Mohammad Nurdin Prastya Hermansah <108654517+MohPras@users.noreply.github.com>
2025-06-11 11:32:55 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -710,7 +710,7 @@ document.addEventListener("DOMContentLoaded", () => {
         formData.append("file", file);
 
         try {
-            const response = await fetch("http://localhost:5000/predict", {
+            const response = await fetch("http://localhost:8000/predict", {
                 method: "POST",
                 body: formData
             });
